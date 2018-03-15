3c2d212a7531a3e02b5621720cd1aed6a5324d7d
jstansfield0 <stansfieldjc@vcu.edu>
2018-03-15 14:45:23 +0000

    compile supplemental

--- supplementary/merge_supplementary.sh
+++ supplementary/merge_supplementary.sh
@@ -1,5 +1,5 @@
 # Merge PDFs
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=HiCcompare_supplementary.pdf 01_Methods.pdf 02_Distance.pdf 03_Biases.pdf 04_Differential.pdf 05_diffHiC.pdf
+gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=HiCcompare_supplementary.pdf 00_tableofcontents.pdf 01_Methods.pdf 02_Distance.pdf 03_Biases.pdf 04_Differential.pdf 05_diffHiC.pdf 06_FIND.pdf
 # Compress the resulting file - makes no difference
 # compresspdf HiCcompare_supplementary.pdf HiCcompare_supplementary_1.pdf
 
